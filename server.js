4317f1fd6bc747602aec6da2a6142b931f91f413
Anne Mabire <54441893+annebea@users.noreply.github.com>
2020-06-22 10:14:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,22 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/timestamp/", (req, res) => {
+  res.json({unix: Date.now(), utc: Date() });
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  
+  if (/\d{5,}/.test(dateString)) {
+    var dateInt = parseInt(dateString);
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+  let dateObject = new Date(dateString);
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date"});
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+})
